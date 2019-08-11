8eb1e136858a67e3258cf8a7687b9f922abd9030
Nikhil Prasad <nikp11396@gmail.com>
2019-08-12 01:55:35 +0530

    increase number of data points

--- train_model.py
+++ train_model.py
@@ -2,7 +2,8 @@ import time, tensorflow as tf, numpy as np, matplotlib.pyplot as plt, mancala
 
 game = mancala.Mancala()
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(14, activation = tf.nn.softmax, input_shape=(14,))
+    tf.keras.layers.Dense(28, activation = tf.nn.relu, input_shape=(14,)),
+    tf.keras.layers.Dense(14, activation = tf.nn.softmax)
      ])
 
 optimizer = tf.keras.optimizers.SGD(0.01)
@@ -18,10 +19,11 @@ def generate_data(size):
         op.append(game.get_best_pit())
     return np.array(ip, ndmin=2), np.array(op,ndmin=2)
 
-data_size = 1000
+data_size = 100000
 ips, ops = generate_data(data_size)
 history = model.fit(ips,ops,batch_size=data_size,epochs=epochs,validation_split=0.2,verbose=1)
 
+model.save('new model', save_format='h5')
 fig = plt.figure()
 p1 = fig.add_subplot(121)
 p2 = fig.add_subplot(122)
