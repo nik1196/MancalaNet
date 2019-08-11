f762270a97bc3c86471b414cef1da3e9d6a8f154
nik1196 <nikp11396@gmail.com>
2019-08-11 23:14:00 +0530

    Add files via upload

---
+++ mancala.py
@@ -0,0 +1,40 @@
+import random
+class Mancala:
+    def __init__(self):
+        self.pits = [5 for i in range(14)]
+        random.seed()
+    def play(self, pit_num, gain=0):  
+        pit = pit_num - 1
+        next_pit = (pit + self.pits[pit]+1) % 14
+        num_iter = self.pits[pit]
+        self.pits[pit] = 0
+        for i in range(pit+1, pit+num_iter+1):
+            self.pits[i%14] += 1
+        if self.pits[next_pit] > 0:
+            return gain + self.play(next_pit+1, gain)
+        score = self.pits[(next_pit+1) % 14]
+        self.pits[(next_pit+1) % 14] = 0
+        return score
+
+    def get_best_pit(self):
+        max_score = 0
+        best_pit = 0
+        for pit in range(6,len(self.pits)):
+            if self.pits[pit] == 0:
+                continue
+            pits_temp = list(self.pits)
+            score = self.play(pit+1)
+            if score > max_score:
+                max_score = score
+                best_pit = pit + 1
+            self.pits = list(pits_temp)
+        return [ 1 if i == best_pit else 0 for i in range(len(self.pits))]
+
+    
+    def generate_state(self):
+        total_seeds = 70
+        for i in range(14):
+            self.pits[i] = random.randrange(0,total_seeds+1)
+            total_seeds -= self.pits[i]
+        return self.pits
+    
---
+++ train_model.py
@@ -0,0 +1,40 @@
+import time, tensorflow as tf, numpy as np, matplotlib.pyplot as plt, mancala
+
+game = mancala.Mancala()
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(14, activation = tf.nn.softmax, input_shape=(14,))
+     ])
+
+optimizer = tf.keras.optimizers.SGD(0.01)
+
+model.compile(loss=tf.keras.losses.categorical_crossentropy.__name__, optimizer=optimizer,metrics=['accuracy'])
+epochs = 200
+
+def generate_data(size):
+    ip = []
+    op = []
+    for i in range(size):
+        ip.append(game.generate_state())
+        op.append(game.get_best_pit())
+    return np.array(ip, ndmin=2), np.array(op,ndmin=2)
+
+data_size = 1000
+ips, ops = generate_data(data_size)
+history = model.fit(ips,ops,batch_size=data_size,epochs=epochs,validation_split=0.2,verbose=1)
+
+fig = plt.figure()
+p1 = fig.add_subplot(121)
+p2 = fig.add_subplot(122)
+p2.set_ylim(0,1)
+p2.grid()
+p1.grid()
+p2.set_yticks(np.arange(0,1,0.1))
+x = [i for i in range(epochs)]
+y = history.history['loss']
+y2 = history.history['acc']
+p1.plot(x,y, 'r', label='loss')
+p1.legend()
+p2.plot(x,y2, 'b', label='accuracy')
+p2.legend()
+plt.show()
+
